af62708e85f962830a2f4710c830e1267a2c0b13
cglaze <56330445+cglaze11@users.noreply.github.com>
2023-09-25 21:30:45 +0800

    Update windowbatch.bat

--- Method/windowbatch.bat
+++ Method/windowbatch.bat
@@ -1,6 +1,6 @@
 
 set path=D:\Documents\SASsupport\China Submission\Programs\TLGprg\
 
-set start= D:\Sofeware\SASHome\SASFoundation\9.4\sas.exe -CONFIG "D:\Sofeware\SASHome\SASFoundation\9.4\nls\u8\sasv9.cfg" /nosplash /batch /autoexec "D:\Documents\SASsupport\China Submission\Programs\_Global\00_Setup.sas" /nolog /NOPRINT
+set start= D:\Sofeware\SASHome\SASFoundation\9.4\sas.exe -CONFIG "D:\Sofeware\SASHome\SASFoundation\9.4\nls\u8\sasv9.cfg" /nosplash /batch /autoexec "D:\Documents\SASsupport\China Submission\Programs\_Global\00_Setup.sas" /nolog  /NOPRINT
 
-%start% /sysin '%path%L-16-01a-V1-20AUG2022-8SEP2022.sas'
+%start% /sysin '%path%L-16-01a-V1-20AUG2022-8SEP2022.sas' /log '%path%Outputs\'
